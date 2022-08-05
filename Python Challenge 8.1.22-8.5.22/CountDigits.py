b3a519ee830d02f8c89f510becfd726d17bf6178
Sophia <bhullarsophia@gmail.com>
2022-08-05 11:59:07 -0400

    final commit

--- Python Challenge 8.1.22-8.5.22/CountDigits.py
+++ Python Challenge 8.1.22-8.5.22/CountDigits.py
@@ -1,27 +1,14 @@
 
-
-count = 0
-
 def digits_count(num):
-    global count
 
-    if(num!=0):
-        count+=1
-        return digits_count(num//10)
+    if(num<=9):
+        return 1
+    else:
+        return 1 + digits_count(num/10)
 
-    return count
 
-num1=4666
-num2=544
-num3=121317
-num4=0
-num5=12345
-num6=1289396387328
-print("Count of digits in ",num1, " is ",digits_count(num1))
-# print("Count of digits in ",num2, " is ",digits_count(num2))
-# print("Count of digits in ",num3, " is ",digits_count(num3))
-# print("Count of digits in ",num4, " is ",digits_count(num4))
-# print("Count of digits in ",num5, " is ",digits_count(num5))
-# print("Count of digits in ",num6, " is ",digits_count(num6))
+number=[4666,544,121317,0,12345,1289396387328]
+for x in number:
+    print("Count of digits in ",x, " is ",digits_count(x))
 
 
